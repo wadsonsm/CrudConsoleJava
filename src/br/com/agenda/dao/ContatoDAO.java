a7790d85d6d74c208d60122be7d30f60af921ede
wadson <wadsonmatos@yahoo.com.br>
2021-11-11 17:06:08 -0300

    método select

--- src/br/com/agenda/dao/ContatoDAO.java
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -1,11 +1,16 @@
 package br.com.agenda.dao;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mysql.jdbc.PreparedStatement;
 import br.com.agenda.factory.ConnectionFactory;
 import br.com.agenda.model.Contato;
 
-public class ContatoDAO {
+public class ContatoDAO
+{
 
 	public void save(Contato contato)
 	{
@@ -42,4 +47,58 @@ public class ContatoDAO {
 			}
 		}
 	}
-}
+
+
+	@SuppressWarnings("finally")
+	public List<Contato> getContatos()
+	{
+		String sql = "SELECT * FROM CONTATOS";
+		
+		List<Contato> contatos = new ArrayList<Contato>();
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		//classe que vai recuperar os dados do banco ** SELECT **
+		ResultSet rst = null;
+		
+		try {
+			conn = ConnectionFactory.createConnectionToMySql();
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			rst = pstm.executeQuery();
+			
+				while (rst.next())
+				{
+					Contato contato = new Contato();
+					//recuperar id
+					contato.setId(rst.getInt("id"));
+					//recupear nome
+					contato.setNome(rst.getString("nome"));
+					// recuperar a idade
+					contato.setIdade(rst.getInt("idade"));
+					//recuperar a data de cadastro
+					contato.setDataCadastro(rst.getDate("dataCadastro"));
+					
+					contatos.add(contato);			
+				}
+			
+			}catch (Exception e){
+				e.printStackTrace();
+			}finally{	
+				try {
+					if(pstm != null) {
+						pstm.close();
+					}
+					
+					if(conn != null){
+						conn.close();
+					}
+				}catch (Exception e2) {
+						e2.printStackTrace();
+					}	
+				return contatos;
+	}}}
+	
+
+
+
+
